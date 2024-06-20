2390f06f6157314fa5a86b96d1ba217d6649392c
Samuel Jonathan JOSEPH <isteah.josephsamuel@gmail.com>
2024-06-20 16:30:35 -0400

    Étape 1 : Initialisation et Configuration de Base🦾

---
+++ main.py
@@ -0,0 +1,12 @@
+# INF2300 Infographie
+# ÉTÉ 2024
+# 
+# Travail No. [1]
+# 
+# Groupe
+# [isteah.josephsamuel@gmail.com] – [JOSEPH Samuel Jonathan]
+# [isteah.jpierrelouis03@gmail.com] – [JONATHAN Pierre Louis]
+# 
+# Soumis au : Dre. Franjieh El Khoury
+# 
+# [6/8/2024]
